37d860d8ad50ac3e4bcad9f9d988350b85d1c53d
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2024-08-14 18:32:43 -0400

    oops

--- src/lib.rs
+++ src/lib.rs
@@ -127,7 +127,7 @@ pub(crate) const MAX_PKT_BUF_SIZE: usize = 1300;
 pub(crate) const CONNECTION_TIMEOUT_SEC: i32 = 15;
 pub(crate) const PACKET_SEND_RATE_SEC: f64 = 1.0 / 10.0;
 
-pub use crate::client::NetcodeSocket;
+pub use crate::socket::NetcodeSocket;
 pub use crate::client::{Client, ClientConfig, ClientState};
 pub use crate::crypto::{generate_key, try_generate_key, Key};
 pub use crate::error::{Error, Result};
